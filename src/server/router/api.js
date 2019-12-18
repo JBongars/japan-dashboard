ddab07dce0acb788097bb6b9b1e0678f961fd468
Julien Bongars <julien.bongars@axa.com.sg>
2019-12-18 23:16:09 +0800

    feat: remove dependency of external api (for demo only)

--- src/server/router/api.js
+++ src/server/router/api.js
@@ -1,22 +1,35 @@
 const express = require("express");
-const HttpProxy = require("http-proxy");
 
 const router = express.Router();
-const config = require("../config");
-const logger = require("../logger");
+
+const Prefecture = require("../Seeds/Prefecture.json");
+const PrefecturePopulation = require("../Seeds/PrefecturePopulation.json");
+const PrefecturePopulationAll = require("../Seeds/PrefecturePopulationAll.json");
 
 // eslint-disable-next-line new-cap
-const proxy = new HttpProxy.createProxyServer();
+// const proxy = new HttpProxy.createProxyServer();
 
-router.use("/prefecture", (req, res) => {
-  logger.info(`Redirecting request to ${config.prefectureApi.host}`);
-  return proxy.web(req, res, { target: `${config.prefectureApi.host}` });
+// router.use("/prefecture", (req, res) => {
+//   logger.info(`Redirecting request to ${config.prefectureApi.host}`);
+//   return proxy.web(req, res, { target: `${config.prefectureApi.host}` });
+// });
+
+router.get("/prefecture", (req, res) => {
+  return res.status(200).json({ result: Prefecture });
 });
 
-router.get("/hello", (req, res) => {
-  logger.info("hit!");
+router.get("/prefecture/all", (req, res) => {
+  return res.status(200).json({ result: PrefecturePopulationAll });
+});
 
-  res.status(200).send("ok");
+router.get("/prefecture/:iso", (req, res) => {
+  const { iso } = req.params;
+
+  const result = PrefecturePopulation.filter(
+    elem => elem.prefectureIso === iso
+  );
+
+  return res.status(200).json({ result });
 });
 
 module.exports = router;
